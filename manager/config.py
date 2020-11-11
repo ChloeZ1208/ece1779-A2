5bce4417112c9344a3502ac8bbbc846c0818529e
ChloeZ1208 <66364747+ChloeZ1208@users.noreply.github.com>
2020-11-11 23:02:03 +0800

    Update config.py
    image id(not created, user-app is not ready now...)

--- manager/config.py
+++ manager/config.py
@@ -1,13 +1,15 @@
 subnet_id = 'subnet-db57afea'
 ami_id = 'ami-0bf618774e7879c6a'
 
-#instance  id = i-045d51d784601f821
-image_id = 'ami-00ad3d66a1f04363d'
+#instance  user1
+image_id = ''
 Iam_profile={
     'Name': 'ece1779a2'
 }
 SecurityGroup_id = ['sg-05428fb146ee292de']
 key_name = 'a2worker0'
+# define userdata to run user-app at instance launch
+user_data = "#!/bin/bash\n./home/ubuntu/Desktop/start.sh \n"
 
 #elb target group ARN
-ARN = 'arn:aws:elasticloadbalancing:us-east-1:327200236258:targetgroup/ece1779a2/87939a9fbfc90979'
+ARN = 'arn:aws:elasticloadbalancing:us-east-1:327200236258:targetgroup/ece1779a2user/da2f671e55d83fc0'
