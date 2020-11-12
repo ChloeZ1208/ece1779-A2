7ee4f87d2ec5f52cd3553ec9e7f5f7e681d20dcd
ChloeZ1208 <66364747+ChloeZ1208@users.noreply.github.com>
2020-11-12 23:15:25 +0800

    Update config.py

--- manager/config.py
+++ manager/config.py
@@ -3,13 +3,23 @@ ami_id = 'ami-0bf618774e7879c6a'
 
 #instance  user1
 image_id = ''
-Iam_profile={
-    'Name': 'ece1779a2'
-}
+Iam_profile = { 'Name': 'ece1779a2'}
 SecurityGroup_id = ['sg-05428fb146ee292de']
 key_name = 'a2worker0'
+
 # define userdata to run user-app at instance launch
-user_data = "#!/bin/bash\n./home/ubuntu/Desktop/start.sh \n"
+user_data = "#!/bin/bash\n screen\n /home/ubuntu/Desktop/ece1779a1/start.sh"
 
 #elb target group ARN
-ARN = 'arn:aws:elasticloadbalancing:us-east-1:327200236258:targetgroup/ece1779a2user/da2f671e55d83fc0'
+targetgroup = 'targetgroup/ece1779a2user/da2f671e55d83fc0'
+targetgroupARN = 'arn:aws:elasticloadbalancing:us-east-1:327200236258:targetgroup/ece1779a2user/da2f671e55d83fc0'
+#elb
+loadbalancer = 'app/ece1779a2/5f33ad35c054e2bb'
+loadbalancerARN = 'arn:aws:elasticloadbalancing:us-east-1:327200236258:loadbalancer/app/ece1779a2/5f33ad35c054e2bb'
+loadbalancerDNS = 'ece1779a2-1322249347.us-east-1.elb.amazonaws.com'
+
+# RDS - database
+db_config = {'user': '',
+             'password': '',
+             'host': '',
+             'database': ''}
