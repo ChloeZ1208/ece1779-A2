3fe2e6e5c7f70b802868d09051edbc7c2ac9fe65
ChloeZ1208 <66364747+ChloeZ1208@users.noreply.github.com>
2020-11-13 21:10:14 +0800

    Update config.py

--- manager/config.py
+++ manager/config.py
@@ -1,16 +1,33 @@
 subnet_id = 'subnet-db57afea'
 ami_id = 'ami-0bf618774e7879c6a'
-
+#manager
+manager_id = 'i-045d51d784601f821'
 #instance  user1
-image_id = ''
+image_id = 'ami-0620b60a074c81804'
 Iam_profile = { 'Name': 'ece1779a2'}
 SecurityGroup_id = ['sg-05428fb146ee292de']
 key_name = 'a2worker0'
 
 # define userdata to run user-app at instance launch
-user_data = "#!/bin/bash\n screen\n /home/ubuntu/Desktop/ece1779a1/start.sh"
-'''Try this if it does not work:  
-Content-Type: multipart/mixed; boundary="//" MIME-Version: 1.0 --// Content-Type: text/cloud-config; charset="us-ascii" MIME-Version: 1.0 Content-Transfer-Encoding: 7bit Content-Disposition: attachment; filename="cloud-config.txt" #cloud-config cloud_final_modules: - [scripts-user, always] --// Content-Type: text/x-shellscript; charset="us-ascii" MIME-Version: 1.0 Content-Transfer-Encoding: 7bit Content-Disposition: attachment; filename="userdata.txt" #!/bin/bash screen /home/ubuntu/Desktop/ece1779a1/start.sh --//'''
+user_data = 'Content-Type: multipart/mixed; boundary="//"\n' \
+            'MIME-Version: 1.0\n' \
+            '--//\n' \
+            'Content-Type: text/cloud-config; charset="us-ascii"\n' \
+            'MIME-Version: 1.0\n' \
+            'Content-Transfer-Encoding: 7bit\n' \
+            'Content-Disposition: attachment; filename="cloud-config.txt"\n' \
+            '#cloud-config\n' \
+            'cloud_final_modules:\n' \
+            '- [scripts-user, always]\n' \
+            '--//\n' \
+            'Content-Type: text/x-shellscript; charset="us-ascii"\n' \
+            'MIME-Version: 1.0\n' \
+            'Content-Transfer-Encoding: 7bit\n' \
+            'Content-Disposition: attachment; filename="userdata.txt"\n' \
+            '#!/bin/bash\n' \
+            'screen\n' \
+            '/home/ubuntu/Desktop/ece1779a1/start.sh\n' \
+            '--//'
 
 #elb target group ARN
 targetgroup = 'targetgroup/ece1779a2user/da2f671e55d83fc0'
@@ -21,7 +38,7 @@ loadbalancerARN = 'arn:aws:elasticloadbalancing:us-east-1:327200236258:loadbalan
 loadbalancerDNS = 'ece1779a2-1322249347.us-east-1.elb.amazonaws.com'
 
 # RDS - database
-db_config = {'user': '',
-             'password': '',
-             'host': '',
+db_config = {'user': 'admin -pece1779a2',
+             'password': '3306',
+             'host': 'ece1779.cfgoq3p3aw7m.us-east-1.rds.amazonaws.com',
              'database': ''}
