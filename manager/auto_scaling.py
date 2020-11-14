c3609e138b8e3d7b9beafb5f7857c7246734bc50
ChloeZ1208 <66364747+ChloeZ1208@users.noreply.github.com>
2020-11-14 23:10:58 +0800

    Update auto_scaling.py

--- manager/auto_scaling.py
+++ manager/auto_scaling.py
@@ -1,10 +1,9 @@
-from flask import render_template
+from flask import render_template,g
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, IntegerField, FloatField
 from wtforms.validators import DataRequired
-import boto3
 import mysql.connector
-from manager import admin, check_cpu
+from manager import admin, config
 
 class AutoScalingForm(FlaskForm):
 	grow_threshold = IntegerField('CPU Threshold for Growing the Worker Pool',
@@ -19,12 +18,14 @@ class AutoScalingForm(FlaskForm):
 
 '''
 Functions for database
+'''
 
 def connect_to_database():
-	return mysql.connector.connect(user=db_config['user'],
-					password=db_config['password'],
-					host=db_config['host'],
-					database=db_config['database'])
+	return mysql.connector.connect(user=config.db_config['user'],
+									password=config.db_config['password'],
+									host=config.db_config['host'],
+									ssl_ca=config.db_config['ssl_ca'],
+									database=config.db_config['database'])
 
 def get_db():
 	db = getattr(g, '_database', None)
@@ -38,7 +39,6 @@ def teardown_db(exception):
 	db = getattr(g, '_database', None)
 	if db is not None:
 		db.close()
-'''
 
 
 '''
@@ -52,30 +52,20 @@ def auto_scaling():
 		shrink_threshold = form.shrink_threshold.data
 		expand_ratio = form.expand_ratio.data
 		shrink_ratio = form.shrink_ratio.data
-		"""
+
 		cnx = get_db()
-        	cursor = cnx.cursor()
-        	query = '''UPDATE policy
-                   	SET grow_th = %s,
-                       	    shrink_th = %s,
-                            expand_ratio = %s,
-                            shrink_ratio = %s
-                            WHERE id = 1
-        	'''
-        	cursor.execute(query, (growth_threshold,shrink_threshold,expand_ratio,shrink_ratio))
-        	cnx.commit()
-		compare_cpu()
-		"""
-	return render_template('auto_scaling.html', form=form)
+		cursor = cnx.cursor()
+		query = '''UPDATE autoScaling
+					SET grow_th = %s,
+						shrink_th = %s,
+						expand_ratio = %s,
+						shrink_ratio = %s
+					WHERE id = 1
+		'''
+		cursor.execute(query, (grow_threshold,shrink_threshold,expand_ratio,shrink_ratio))
+		cnx.commit()
+
+	return render_template('ec2_list.html', form=form)
 
 
-"""
-def get_policy():
-    cnx = get_db()
-    cursor = cnx.cursor()
-    query = '''SELECT * FROM policy'''
-    cursor.execute(query)
-    row = cursor.fetchone()
-    return row
-"""
 
